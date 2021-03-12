63f547e50795da916f78effe27b6edf86ea1b2df
Guillermo A <lgar89@gmail.com>
2021-03-12 00:25:39 -0600

    initial commit - first test case copied from testweave-sdk repo

---
+++ tests/_init_arweave.ts
@@ -0,0 +1,20 @@
+import Arweave from 'arweave';
+
+const arweave = Arweave.init({
+  host: 'localhost',
+  port: 1984,
+  protocol: 'http',
+  timeout: 20000,
+  logging: false,
+});
+
+// production net
+/* const arweaveInstace = Arweave.init({
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https',
+  timeout: 20000,
+  logging: false,
+}); */
+
+export default arweave;
